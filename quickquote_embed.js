47dc0edfb03ed1d3b6c5eb5b60f9a5e57c22c7d5
pontuslagerberg <55412769+pontuslagerberg@users.noreply.github.com>
2025-12-10 01:28:05 +0100

    Update quickquote_embed.js

--- quickquote_embed.js
+++ quickquote_embed.js
@@ -214,8 +214,7 @@ window.addEventListener('load', function () {
   WS_iframeResizeReady(function (resize) {
     resize(
       {
-        sizeHeight: true,
-        sizeWidth: true,
+        direction: 'both',
         license: 'GPLv3',
         log: false
       },
